8039e908b649ac804253423bfb61e307d3e8ae26
Kn51 <71325645+kn51@users.noreply.github.com>
2020-11-21 21:32:34 +0400

    database
    Created connection.

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <title>iDragon - JavaScript Game</title>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </head>
 <body>
     <div class="gameContainer">
-        <div class="gameOver">Welcome to Adventures</div>
-        <!-- <div class="gameOver">Welcome to iDragon - Created by RCF</div> -->
+        <div class="gameOver">Welcome to iDragon Adventures</div>
+        <!-- <div class="gameOver">Welcome to iDragon - Created by Harry</div> -->
         <div class="dino"></div>
         <div id="scoreCont">Your Score: 0</div>
+        <div id="leaderboard"><a href="leaderboard.php">Goto Leaderboard</a></div>
         <div class="obstacle obstacleAni"></div>
     </div>
 </body>
