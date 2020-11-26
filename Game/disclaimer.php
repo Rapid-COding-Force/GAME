5340cb629f1416beb86cfe28e48b2fc3c2ef1db0
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-26 07:04:54 +0400

    Create disclaimer.php

---
+++ Game/disclaimer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disclaimer</title>
+    <link rel="stylesheet" href="game.css">
+</head>
+<body>
+<div id="gameContainer">
+<button id="menu" class="button button5"><a href="menu.php">Back to Main Menu</a></button>
+<div id="text">
+    <p><h1>Disclaimer:</h1><br>
+        <h3>This game contains fast moving objects</h3>
+    </p>
+</div>
+</div>
+</body>
+</html>
