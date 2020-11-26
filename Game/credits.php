1db06c4dfc02f17641f0b86769b1da01d2e2fd17
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-26 07:04:18 +0400

    Create credits.php

---
+++ Game/credits.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Credits</title>
+    <link rel="stylesheet" href="game.css">
+</head>
+<body>
+<div id="gameContainer">
+<button id="menu" class="button button5"><a href="menu.php">Back to Main Menu</a></button>
+<div id="text">
+    <p><h1>Credits</h1><br>
+        <h2>Rapid-Coding-Force Group:</h2><br>
+        <h4>Azzaam Waheed<br>
+            Hadiqa Qureshi<br>
+            Taha Khan<br>
+            Mohammad Atif<br>
+            Kezia Nathaniel</h4>
+    </p>
+</div>
+</div>
+</body>
+</html>
