fb2bdca79433e7381440bdec16772e7e6094a201
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-26 07:08:01 +0400

    Update menu.php

--- Game/menu.php
+++ Game/menu.php
@@ -8,8 +8,12 @@
 </head>
 <body>
 <div id="gameContainer">
-<button id="startGame" class="button button1"><a href="game.php">Start Game</a></button>
-<button id="leaderboard" class="button button4" ><a href="leaderboard.php">Go to Leaderboard</a></button>
+<button id="startGame" class="button button5"><a href="game.php">Start Game</a></button>
+<button id="leaderboard" class="button button6" ><a href="leaderboard.php">Go to Leaderboard</a></button>
+<button id="howToPlay" class="button button6" ><a href="howToPlay.php">How to Play</a></button>
+<button id="disclaimer" class="button button6" ><a href="disclaimer.php">Disclaimer</a></button>
+<button id="credits" class="button button6" ><a href="credits.php">Credits</a></button>
+<div id="dinoDash"><br><br><br><h1>Dino Dash<br><h6>by Rapid-Coding-Force</h5></h1></div>
 </div>
 </body>
 </html>
