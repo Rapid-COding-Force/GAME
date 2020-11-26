6ecce3dc3fbe8d314c28683d9d154b517ae9ee98
Azzaam Waheed <71337584+an20006@users.noreply.github.com>
2020-11-26 07:06:49 +0400

    Create howToPlay.php

---
+++ Game/howToPlay.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disclaimer</title>
+    <link rel="stylesheet" href="game.css">
+</head>
+<body>
+<div id="gameContainer">
+<button id="menu" class="button button5"><a href="menu.php">Back to Main Menu</a></button>
+<div id="text">
+    <p><h1>About:</h1><br>
+        <h3>Dino Dash is a game inspired by the offline dino chrome game.
+            It utilizes the same ideas but implements a leaderboard system, different menus and
+            an additional movement function to the dino; the duck movement
+        </h3><br>
+        <h1>Controls:</h1><br>
+        <h3>Jump Keys - W, Up Arrow Key, SpaceBar<br>
+            Duck Keys - S, Down Arrow Key
+        </h3>
+    </p>
+</div>
+</div>
+</body>
+</html>
